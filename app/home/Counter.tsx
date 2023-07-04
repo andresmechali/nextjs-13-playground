6adef25241ca91bb84498f70eeaebded13a43a6c
Andres Mechali <andresmechali@gmail.com>
2023-07-04 19:53:03 +0200

    add counter to home page

---
+++ app/home/Counter.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { useState } from "react";
+export default function Counter() {
+  const [count, setCount] = useState<number>(0);
+  return (
+    <div className="flex flex-col space-y-4">
+      <div>
+        Count: <span className="underline">{count}</span>
+      </div>
+      <div className="flex flex-row space-x-4">
+        <button
+          className="border-2 border-orange-300 p-2 hover:bg-orange-300 hover:text-black"
+          onClick={() => setCount(count + 1)}
+        >
+          Add 1
+        </button>
+        <button
+          className="border-2 border-orange-300 p-2 hover:bg-orange-300 hover:text-black"
+          onClick={() => setCount(0)}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+import Counter from "@/app/home/Counter";
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="mb-8 font-bold">Home page</h1>
+      <Counter />
+    </div>
+  );
+}
